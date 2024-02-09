74f828ee62389e603efda54bb753012614f9695c
Geona Kim (MacOS) <rtguna@icloud.com>
2024-02-09 16:42:53 +0900

    chore : Note::draw() 임시 이미지 추가

--- lib/msig_Note_draw.cpp
+++ lib/msig_Note_draw.cpp
@@ -4,7 +4,16 @@ namespace msig
 {
 
 
-Mat Note::draw(){return Mat();}
+Mat Note::draw(){
+    // 3채널 하얀 이미지(Mat) 생성
+    Mat img(512, 512, CV_8UC3, Scalar(255, 255, 255));
+    // Point 생성
+    Point2i pt1(50, 50), pt2(462, 462);
+    // Mat에 rectangle 그리기
+    rectangle(img, pt1, pt2, Scalar(0, 255, 0));
+    // 생성된 이미지 리턴
+    return img;
+}
 
 
 }
