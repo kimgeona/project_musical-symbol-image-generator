19e01eaf018a5c1c2cd0574d06ba00056c13f4f1
Geona Kim (MacOS) <rtguna@icloud.com>
2024-02-09 17:13:25 +0900

    chore : Note::set() 함수 구현 완료.

--- lib/msig_Note_set.cpp
+++ lib/msig_Note_set.cpp
@@ -5,7 +5,23 @@ namespace msig
 {
 
 
-void Note::set(string key, string value){}
+void Note::set(string key, string value){
+    // key bad.
+    if (data.find(key) == data.end()){
+        cout << "msig: Bad access. key:" << key << " is not exist." << endl;
+        return;
+    }
+    // key good, value good.
+    else if (regex_match(value, data_reg_exp[key])){
+        data['key'] = value;
+        return;
+    }
+    // key good, value bad.
+    else {
+        cout << "msig: Invalid argument input. Please check the input format according to the key:" << key << "." << endl;
+        return;
+    }
+}
 
 
 }
