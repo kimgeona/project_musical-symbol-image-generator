4635dcb107ddf5943b6ccae08f3cbd03bcbda3c3
Geona Kim (MacOS) <rtguna@icloud.com>
2024-02-10 13:28:51 +0900

    chore : 테스트 확인을 위한 main.cpp 수정.

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,11 @@ int main(){
     msig::Note n = msig::Note();
     
     // set
-    n.set("pitch", "a4");     // 음정 = 라
-    n.set("head", "4");       // 머리 = 4분음표
-    n.set("stem", "3");       // 기둥 = 머리크기 3배
+    n.set("pitch", "a4_#");     // 음정 = 라#
+    n.set("head", "4");         // 머리 = 4분음표
+    n.set("stem", "3.");        // 기둥 = 머리크기의 3.0배
+    n.set("tail", "8");         // 꼬리 종류 = 일반
+    n.set("dot", "dot");        // 점 표시
     
     // save as image
     n.save_as_img("test.png");
