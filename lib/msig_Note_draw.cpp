4aee6fa43fc0ccabf351d8e95312cbcfb2d97381
Geona Kim (MacOS) <rtguna@icloud.com>
2024-03-08 22:51:20 +0900

    chore : 새로운 악상 기호 추상화 작업에 따른 코드 수정 3.

--- lib/msig_Note_draw.cpp
+++ lib/msig_Note_draw.cpp
@@ -12,10 +12,10 @@ cv::Mat Note::draw(){
     Mat img(img_h, img_w, CV_8UC1, Scalar(255));
     
     // draw_list에 있는 악상 기호들 img에 그리기
-    for(auto key : draw_list){
+    for(auto ms : draw_list){
         // 이미지와 config 불러오기
-        Mat img_symbol = img_symbols[key.first].clone();    // 악상 기호 이미지
-        string img_config = key.second;                     // 악상 기호 config
+        Mat     img_symbol = ms.img.clone();
+        string  img_config = to_string(ms.x)+"_"+to_string(ms.y)+"_"+to_string(ms.rotate)+"_"+to_string(ms.scale);
         
         // 악상 기호 그리기
         img = draw_symbols(img, img_symbol, img_config);
@@ -35,12 +35,12 @@ cv::Mat Note::draw_symbols(const cv::Mat& img, const cv::Mat& img_symbol, std::s
     Mat img2 = img_symbol.clone();  // 행렬 복사
     
     // 편집 정보 추출
-    vector<string> configs = split(img_config, "_"); // "x_y_각도_확대축소_대칭"
+    vector<string> configs = my_split(img_config, "_"); // "x_y_각도_확대축소_대칭"
     
     // 이미지 편집(회전, 확대 및 축소, 대칭)
     img2 = rotate_mat(img2, stod(configs[2]), img2.cols/2.0, img2.rows/2.0);
     img2 = scale_mat(img2, stod(configs[3]));
-    img2 = symmetry_mat(img2, configs[4]);
+    //img2 = symmetry_mat(img2, configs[4]);
     
     // 이미지 합성 좌표 계산
     int x = (img_w)/2.0 - (img2.cols/2.0) + (stoi(configs[0]));   // 이미지 중심 - sub 이미지 중심 + config 값
