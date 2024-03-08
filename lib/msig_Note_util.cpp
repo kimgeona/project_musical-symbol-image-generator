9be2ddbefaaa56a0c3442c3b5d5d37b5dbf95e1e
Geona Kim (MacOS) <rtguna@icloud.com>
2024-03-08 22:41:15 +0900

    chore : 새로운 악상 기호 추상화 작업에 따른 코드 수정 1.

--- lib/msig_Note_util.cpp
+++ lib/msig_Note_util.cpp
@@ -4,6 +4,50 @@ namespace msig
 {
 
 
+// 의존성 추가 위치 알아내기
+std::vector<std::string>    Note::get_list_add(std::string dir){
+    return std::vector<std::string>();
+}
+// 의존성 제거 위치 알아내기
+std::vector<std::string>    Note::get_list_delete(std::string dir){
+    return std::vector<std::string>();
+}
+// 악상 기호 이름 알아내기
+std::string                 Note::get_name(std::string dir){
+    using namespace std;
+    using namespace cv;
+    using namespace std::filesystem;
+    
+    path file_dir(dir);
+    return my_split(file_dir.filename(), ".")[0];
+}
+// config 정보 가져오기
+std::vector<std::string>    Note::get_config(std::string line){
+    using namespace std;
+    using namespace cv;
+    
+    line = my_trim(line);
+    line = my_lower(line);
+    line = transform_to_write(line);
+    
+    vector<string> configs;
+    
+    string config_string = my_grep(dir_ds_config, line);
+    
+    if (config_string!=""){
+        config_string = config_string.substr(line.length()+1, config_string.length());
+        string re_i = "([-+]?[0-9]+)";
+        string re_d = "([-+]?([0-9]+.[0-9]*|[0-9]*.[0-9]+))";
+        regex config_reg("^"+re_i+"_"+re_i+"_"+re_d+"_"+re_d+"$");
+        if (regex_match(config_string, config_reg))
+            configs = my_split(config_string, "_");
+    }
+    
+    return configs;
+}
+
+
+
 // 이미지 재생성
 void Note::restore_image(std::string dir){
     using namespace std;
@@ -20,7 +64,7 @@ void Note::restore_image(std::string dir){
     // 이미지 저장
     imwrite(dir, img);
 }
-//이미지 패딩 제거
+// 이미지 패딩 제거
 void Note::remove_padding(std::string dir){
     using namespace std;
     using namespace cv;
@@ -69,181 +113,160 @@ void Note::remove_padding(std::string dir){
 }
 
 
-
-// symbols 무결성 유지
-void Note::integrity_symbols(){
+// 문자열 분리
+std::vector<std::string>    Note::my_split(std::string s1, std::string s2){
     using namespace std;
     using namespace cv;
     
-    // img_symbols에는 있는데 img_configs에는 없는것 -> config 생성
-    for (auto e : img_symbols){
-        string symbol_name = e.first;
-        string symbol_config;
-        
-        // img_configs에서 존재 확인
-        if (img_configs.find(symbol_name)!=img_configs.end()) continue;
-        
-        // config 생성
-        make_config(symbol_name, symbol_config);
-        
-        // symbol_config 확인
-        if (symbol_config=="") break;
-        
-        // config 생성
-        img_configs[symbol_name] = symbol_config;
-    }
+    // 빈 문자열인 경우
+    if (s1.empty()) return std::vector<std::string>();
     
-    // img_symbols에는 있는데 img_configs에는 없는것 -> symbols 삭제
-    vector<string> remove_list;
-    for (auto e : img_symbols){
-        string symbol_name = e.first;
-        
-        // img_configs에서 존재 확인
-        if (img_configs.find(symbol_name)!=img_configs.end()) continue;
-        
-        // symbol 삭제 목록에 추가
-        remove_list.push_back(symbol_name);
+    // s2를 기준으로 s1 분리
+    vector<string> s_out;
+    size_t p = 0;
+    size_t n = s1.find(s2);
+    while (n != string::npos) {
+        s_out.push_back(s1.substr(p, n - p));
+        p = n + 1;
+        n = s1.find(s2, p);
     }
-    for (auto e : remove_list){
-        img_symbols.erase(e);
-    }
+    s_out.push_back(s1.substr(p, s1.length() - p));
+    return s_out;
 }
-
-// configs 무결성 유지
-void Note::integrity_configs(){
+// 문자열 앞,뒤 공백 제거
+std::string                 Note::my_trim(std::string s1){
     using namespace std;
     using namespace cv;
     
-    // img_configs에는 있는데 img_symbols에는 없는것 -> symbol 생성
-    // ..(악상 기호 이미지 생성은 불가능함)
+    // 빈 문자열인 경우
+    if (s1.empty()) return "";
     
-    // img_configs에는 있는데 img_symbols에는 없는것 -> config 삭제
-    vector<string> remove_list;
-    for (auto e : img_configs){
-        string symbol_name = e.first;
-        
-        // img_symbols에서 존재 확인
-        if (img_symbols.find(symbol_name)!=img_symbols.end()) continue;
-        
-        // config 삭제 목록에 추가
-        remove_list.push_back(symbol_name);
-    }
-    for (auto e : remove_list){
-        img_configs.erase(e);
-    }
+    // 왼쪽 공백 조사
+    int left = 0;
+    while (left < s1.length() && std::isspace(s1[left])) left++;
+    
+    // 오른쪽 공백 조사
+    int right = (int)s1.length();
+    while (left < right && std::isspace(s1[right-1])) right--;
+    
+    // 공백이 제거된 문자열 범위 추출
+    return s1.substr(left, right - left);
+}
+// 문자열 소문자로 변환
+std::string                 Note::my_lower(std::string s1){
+    using namespace std;
+    using namespace cv;
+    
+    // 빈 문자열인 경우
+    if (s1.empty()) return "";
+    
+    // 소문자로 변환
+    string s1_copy = s1;
+    for (int i=0; i<s1_copy.length(); i++)
+        if ('A' <= s1_copy[i] && s1_copy[i] <= 'Z')
+            s1_copy[i] += 32;
+    return s1_copy;
 }
 
-// symbol_dataset_config 저장
-void Note::save_symbol_dataset_config(){
+
+// grep 명령어 구현
+std::string Note::my_grep(std::string dir, std::string s){
+    using namespace std;
+    using namespace cv;
+    
+    fstream fin(dir, ios::in);
+    if (!fin){
+        cout << "msig: ";
+        cout << "Note(): ";
+        cout << "my_grep(): ";
+        cout << "can't open " << dir << " for reading.";
+        cout << endl;
+        exit(1);
+    }
+    string line;
+    while (getline(fin, line)) {
+        if (line.find(s)!=-1){
+            fin.close();
+            return line;
+        }
+    }
+    fin.close();
+    return "";
+}
+
+
+// 쓰기 변환
+std::string Note::transform_to_write(std::filesystem::path dir){
     using namespace std;
     using namespace cv;
     using namespace std::filesystem;
     
-    // symbol_dataset_config.txt 경로
-    path config_dir = path(ds_dir)/path("symbol_dataset_config.txt");
-    
-    // 기존 symbol_dataset_config.txt 삭제
-    if (exists(config_dir)) remove(config_dir);
-        
-    // 새로운 symbol_dataset_config.txt 생성
-    fstream fout(config_dir.string(), ios::out|ios::app);
-    if (!fout){
-        cout << "msig: ";
-        cout << "can't open " << config_dir << " for writing.";
-        cout << endl;
-        return;
+    string out = "";
+    for (const auto& e : dir){
+        out += "_" + e.string();
     }
+    out = out.substr(1, out.length());
+    return out;
+}
+// 쓰기 변환
+std::string Note::transform_to_write(std::string dir){
+    using namespace std;
+    using namespace cv;
+    using namespace std::filesystem;
     
-    // config 데이터 저장
-    for (auto e : img_configs){
-        string symbol_name = e.first;
-        string symbol_config = e.second;
-        fout << endl << symbol_name + "=" + symbol_config;
+    return transform_to_write(path(dir));
+}
+// 읽기 변환
+std::string Note::transform_to_read(std::string dir){
+    using namespace std;
+    using namespace cv;
+    using namespace std::filesystem;
+    
+    path out("");
+    for (auto& p : my_split(dir, "_")){
+        out = out / path(p);
     }
-    fout.close();
+    return out.string();
 }
 
-// 문자열 앞,뒤 공백 제거
-void Note::string_trim(std::string& str){
-    // 문자열이 비어있을경우 그냥 종료
-    if (str.empty()) return;
-    
-    // 왼쪽 공백 조사
-    int left = 0;
-    while (left < str.length() && std::isspace(str[left])) left++;
-    
-    // 오른쪽 공백 조사
-    int right = (int)str.length();
-    while (left < right && std::isspace(str[right-1])) right--;
-    
-    // 공백이 제거된 문자열 범위 추출
-    str = str.substr(left, right - left);
-}
 
-// 문자열 소문자로 변환
-void Note::string_to_lower(std::string& str){
+// config값 생성
+std::string Note::make_config(std::string dir){
     using namespace std;
     using namespace cv;
     
-    // 소문자로 변환
-    for (int i=0; i<str.length(); i++)
-        str[i] = tolower(str[i]);
-}
+    // 이미지 생성
+    Mat     img(img_h, img_w, CV_8UC1, Scalar(255));
+    Mat     img_symbol  = imread(dir, IMREAD_GRAYSCALE);
+    string  img_name    = get_name(dir);
+    
+    // config 정보 생성
+    int     x       = 0;
+    int     y       = 0;
+    double  degree  = 0.0;
+    double  scale   = 1.0;
 
-// 문자열 분리
-std::vector<std::string> Note::split(std::string s, std::string c){
-    using namespace std;
-    using namespace cv;
-    
-    vector<string> s_out;
-    size_t p = 0;
-    size_t n = s.find("_");
-    while (n != string::npos) {
-        s_out.push_back(s.substr(p, n - p));
-        p = n + 1;
-        n = s.find("_", p);
-    }
-    s_out.push_back(s.substr(p, s.length() - p));
-    return s_out;
-}
-
-// config 값 생성
-void Note::make_config(std::string symbol_name, std::string& symbol_config){
-    using namespace std;
-    using namespace cv;
-    
-    // 기본 config 정보
-    int x=img_w/2.0;
-    int y=img_h/2.0;
-    double degree=0.0;
-    double scale=1.0;
-    
-    // 이미지 버퍼
-    // ..기존의 config 정보와 달라진게 있을 경우에 이미지를 그리기 위한 mat저장 버퍼
-    
     // 창 생성
-    namedWindow(symbol_name, WINDOW_AUTOSIZE);
+    namedWindow(img_name, WINDOW_AUTOSIZE);
     
     // 키보드 이벤트
     while (true) {
-        // 폅집할 이미지들
-        Mat img(img_h, img_w, CV_8UC1, Scalar(255));
-        Mat img_symbol = img_symbols[symbol_name].clone();
-        string img_config = to_string(x-img_w/2)+"_"+to_string(y-img_h/2)+"_"+to_string(degree)+"_"+to_string(scale)+"_false";
+        Mat img1 = img.clone();
+        Mat img2 = img_symbol.clone();
+        string config = to_string(x)+"_"+to_string(y)+"_"+to_string(degree)+"_"+to_string(scale);
         
         // 이미지 편집
-        img = draw_symbols(img, img_symbol, img_config, true);
+        img1 = draw_symbols(img1, img2, config, true);
         
         // 이미지 화면에 그리기
-        imshow(symbol_name, img);
+        imshow(img_name, img1);
         
         // 키보드 이벤트
-        int key = waitKey(1000/30);
+        int key = waitKey(1000/30);         // 키보드 레이턴시
         if (key=='\n'|| key=='\r') break;   // 완료에 의한 종료
         if (key==27){                       // 중단에 의한 종료
-            destroyWindow(symbol_name);
-            symbol_config = "";
-            return;
+            destroyWindow(img_name);
+            return "";
         }
         switch (key) {
             // 기본 속도
@@ -267,8 +290,8 @@ void Note::make_config(std::string symbol_name, std::string& symbol_config){
             // 초기화
             case 'x':                       // 초기화
             case 'X':                       // 초기화
-                x=img_w/2.0;
-                y=img_h/2.0;
+                x=0;
+                y=0;
                 degree=0.0;
                 scale=1.0;
                 break;
@@ -279,10 +302,43 @@ void Note::make_config(std::string symbol_name, std::string& symbol_config){
         if (degree>360.0) degree -= 360.0;
         if (scale<0.1) scale = 0.1;
     }
-    destroyWindow(symbol_name);
+    destroyWindow(img_name);
     
-    // config 값 저장
-    symbol_config = to_string(x-img_w/2)+"_"+to_string(y-img_h/2)+"_"+to_string(degree)+"_"+to_string(scale)+"_false";
+    // config 값 리턴
+    return to_string(x)+"_"+to_string(y)+"_"+to_string(degree)+"_"+to_string(scale);
+}
+
+
+// config 값 저장(업데이트)
+void Note::save_config(){
+    using namespace std;
+    using namespace cv;
+    using namespace std::filesystem;
+    
+    // 기존 symbol_dataset_config.txt 삭제
+    if (exists(dir_ds_config)) remove(dir_ds_config);
+        
+    // 새로운 symbol_dataset_config.txt 생성
+    fstream fout(dir_ds_config, ios::out|ios::app);
+    if (!fout){
+        cout << "msig: ";
+        cout << "can't open " << dir_ds_config << " for writing.";
+        cout << endl;
+        return;
+    }
+    
+    // config 데이터 저장
+    for (auto& ms : ds){
+        string dir = transform_to_write(ms.dir);
+        string config = to_string(ms.x)+"_"+to_string(ms.y)+"_"+to_string(ms.rotate)+"_"+to_string(ms.scale);
+        fout << endl << dir + "=" + config;
+    }
+    for (auto& ms : ds_piece){
+        string dir = ms.dir;
+        string config = to_string(ms.x)+"_"+to_string(ms.y)+"_"+to_string(ms.rotate)+"_"+to_string(ms.scale);
+        fout << endl << dir + "=" + config;
+    }
+    fout.close();
 }
 
 
