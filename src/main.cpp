d17fa8dadd0a2bc9fb326152950e79a0f5909d58
Geona Kim (MacOS) <rtguna@icloud.com>
2024-04-05 18:21:20 +0900

    test : main 함수 초안 작성.

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,50 @@
 // 나의 라이브러리
 #include <msig.hpp>
 
-int main(){
-    using namespace std;
-    using namespace cv;
-    using namespace std::filesystem;
+using namespace std;
+using namespace cv;
+using namespace std::filesystem;
+
+
+// 데이터셋 주소
+path dataset_dir = path("new-symbol-dataset");
+path dataset_config_dir = dataset_dir / path("symbol_dataset_config.txt");
+
+// 의존적 선택 알고리즘
+msig::DSTree selector;
+
+// 악상기호 조합 클래스
+msig::Canvas canvas;
+
+// 함수 원형들
+void prepare_platform(void);
+void prepare_dataset(void);
+void prepare_DSTree(void);
+void prepare_Canvas(void);
+
+
+// 프로그램
+int main(void)
+{
+    // 0. 플랫폼 준비
+    prepare_platform();
     
+    // 1. 데이터셋 준비
+    prepare_dataset();
+    
+    // 2. 의존적 선택 알고리즘 준비
+    prepare_DSTree();
+    
+    // 3. 악상기호 조합 준비
+    prepare_Canvas();
+    
+    return 0;
+}
+
+
+// 0. 플랫폼 준비
+void prepare_platform(void)
+{
 #ifdef __MACH__
     cout << "Platform : MacOS" << endl << endl;
 #endif
@@ -17,47 +56,48 @@ int main(){
     cout << "Platform : Windows" << endl << endl;
     system("chcp 65001");
 #endif
-    
-    // 데이터셋 주소
-    path dataset_dir = path("new-symbol-dataset");
-    path dataset_config_dir = dataset_dir / path("symbol_dataset_config.txt");
-    
-    
-    // 1. 데이터셋 준비
+}
+
+
+// 1. 데이터셋 준비
+void prepare_dataset(void)
+{
     cout << endl << "1. 데이터셋 준비" << endl;
     for (auto& p : recursive_directory_iterator(dataset_dir)){
-        // 존재하는 파일이고, .png 형식이라면 생성
+        // (존재하는지, 파일인지, .png 확장자인지) 확인
         if (exists(p.path()) && is_regular_file(p.path()) && p.path().extension()==".png"){
             // 악상 기호 생성
             msig::MusicalSymbol ms(p, dataset_config_dir);
-            
             // 악상 기호 상태 체크
-            if (ms.status){
-                cout << "fail    : " << p << endl;
-                continue;
-            }
-            else cout << "success : " << p << endl;
+            if (ms.status)  cout << "fail    : " << p << endl;
+            else            cout << "success : " << p << endl;
         }
     }
-    cout << "완료." << endl;
-    
-    // 2. 의존적 선택 알고리즘 준비
+    cout << "----완료." << endl;
+}
+
+
+// 2. 의존적 선택 알고리즘 준비
+void prepare_DSTree(void)
+{
     cout << endl << "2. 의존적 선택 알고리즘 준비" << endl;
     msig::DSTree selector((dataset_dir/path("complete")).string(), {".png"});
     if (selector==msig::DSTree()){
-        cout << "DSTree 가 생성이 되지 않았습니다." << endl;
-        return -1;
+        cout << "DSTree가 생성이 되지 않았습니다." << endl;
+        exit(-1);
     }
-    cout << "완료." << endl;
-    
-    // 3. 악상기호 조합
-    cout << endl << "3. 악상기호 조합" << endl;
+    cout << "----완료." << endl;
+}
+
+
+// 3. 악상기호 조합 준비
+void prepare_Canvas(void)
+{
+    cout << endl << "3. 악상기호 조합 준비" << endl;
     msig::Canvas canvas(dataset_dir);
     if (canvas==msig::Canvas()){
-        cout << "Canvas 가 생성이 되지 않았습니다." << endl;
-        return -1;
+        cout << "Canvas가 생성이 되지 않았습니다." << endl;
+        exit(-1);
     }
-    cout << "완료." << endl;
-    
-    return 0;
+    cout << "----완료." << endl;
 }
