248f7d786a944bf588fad4b205a8dfe7c4c0e7e0
Geona Kim (MacOS) <rtguna@icloud.com>
2024-04-13 14:09:33 +0900

    test : "악상기호 이미지 그리기(조합)" 테스트 코드 작성 완료.
    (line 47) 에 else 문을 조금더 보완해야 함.
    else 문에 작성된 코드가 곧 악상기호를 배치해주는 코드임.

--- lib/msig_Canvas_Draw.cpp
+++ lib/msig_Canvas_Draw.cpp
@@ -25,47 +25,50 @@ cv::Mat Canvas::draw(){
     // draw_list에 있는 악상 기호들 img에 그리기
     for(auto ms : draw_list)
     {
+        // 악상기호 패딩 제거
+        //remove_padding(ms.img, ms.x, ms.y);
+        
+        // 처음 그려지는 경우
+        if (ms_buff==MusicalSymbol())
+        {
+            ms_buff = ms;                   // 이미지 복사
+            continue;
+        }
+        
         // 독립적 위치
-        if (ms_buff==MusicalSymbol()                                ||
-            ms.dir.parent_path().filename().string()=="line-@"      ||
+        if (ms.dir.parent_path().filename().string()=="line-@"      ||
             ms.dir.parent_path().filename().string()=="edge-@"      ||
             ms.dir.parent_path().filename().string()=="note-up-@"   ||
             ms.dir.parent_path().filename().string()=="note-down-@")
         {
             ms_buff += ms;                  // 이미지 갱신
-            img = draw_symbols(img, ms);    // 이미지 그리기
         }
         
-        /*
-         // 의존적 위치 : 악상기호 위치 조정(아래로 보내기)
-         else
-         {
-             while (ms_buff & ms)
-             {
-                 // 누적 값 반영
-                 ms.y = ms.y - pad;
-                 
-                 // 겹치지 않을 때 까지 내려보내기
-                 while (ms_buff & ms)
-                 {
-                     ms.y = ms.y - 1;
-                     pad = pad + 1;
-                 }
-                 
-                 // 추가 간격 조정
-                 ms.y = ms.y - 7;
-                 pad = pad + 7;
-                 
-                 // 위치 조정 확인
-                 if (ms_buff & ms)   continue;
-                 else                break;
-             }
-             
-             ms_buff += ms;                  // 이미지 갱신
-             img = draw_symbols(img, ms);    // 이미지 그리기
-         }
-         */
+        // 의존적 위치 : 악상기호 위치 조정(아래로 보내기)
+        else
+        {
+            while (true)
+            {
+                // 겹치지 않을 때 까지 내려보내기
+                while (ms_buff & ms)
+                {
+                    ms.y = ms.y - 1;
+                }
+                
+                // 추가 간격 조정
+                ms.y = ms.y - (ms.pad / 2.5);
+                
+                // 겹치는지 확인
+                if (ms_buff & ms)   continue;
+                else                break;
+            }
+            
+            ms_buff += ms;                  // 이미지 갱신
+        }
     }
+    
+    // 악상기호 이미지 그리기
+    img = draw_symbols(img, ms_buff);
     
     // 생성된 이미지 리턴
     return img;
@@ -134,33 +137,29 @@ cv::Mat Canvas::draw_symbols(const cv::Mat& img, const msig::MusicalSymbol& ms, 
     using namespace std;
     using namespace cv;
     
-    // 변수들
-    int     img_w=256;  // 생성할 이미지 너비
-    int     img_h=512;  // 생성할 이미지 높이
-    int     pad = 25;   // 오선지 간격
-    
     // 편집할 이미지들
     Mat img1 = img.clone();         // 행렬 복사
     Mat img2 = ms.img.clone();      // 행렬 복사
     
+    // 악상기호 이미지 중점
+    int cx = ms.x;
+    int cy = ms.y;
     
     // 이미지 편집(회전, 확대 및 축소, 대칭)
-    int tmp_x = ms.x;
-    int tmp_y = ms.y;
-    img2 = mat_rotate(img2, ms.rotate, tmp_x, tmp_y);
-    img2 = mat_scale(img2, ms.scale, tmp_x, tmp_y);
-    
-    // 이미지 합성 좌표 계산
-    int x = (img_w)/2.0 - tmp_x;   // 이미지 중심 - sub 이미지 중심 + config 값
-    int y = (img_h)/2.0 - tmp_y;
+    //img2 = mat_rotate(img2, ms.rotate, cx, cy);
+    img2 = mat_scale(img2, ms.scale, cx, cy);
     
     // 보조선 그리기 (오선지)
     if (auxiliary_line==true){
-        for (auto h : vector<int>({-pad*2, -pad, 0, pad, pad*2})){
+        for (auto h : vector<int>({-ms.pad*2, -ms.pad, 0, ms.pad, ms.pad*2})){
             int n = img1.rows/2.0;
             line(img1, Point(0,n+h), Point(img1.cols,n+h), Scalar(200), 2.2, LINE_AA);
         }
     }
+    
+    // 이미지 합성 좌표 계산
+    int x = (img.cols)/2.0 - cx;   // 이미지 중심 - sub 이미지 중심 + config 값
+    int y = (img.rows)/2.0 - cy;
     
     // 이미지 합성
     img1 = mat_attach(img1, img2, x, y);
