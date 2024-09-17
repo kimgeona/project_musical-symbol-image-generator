3f2b40c4d8157271b1e0cb38c7b115f8ad4fe568
Geona Kim (MacOS) <rtguna@icloud.com>
2024-09-17 17:30:24 +0900

    chore : 기본 데이터셋 구성을 위한 메인프로그램 작성 및 기본 데이터셋 추가.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 // c++17
 #include <iostream>
+#include <filesystem>
 
 // 나의 라이브러리
 #include <msig.hpp>
@@ -13,6 +14,7 @@ void clear()
     system("clear");
 #endif
 }
+
 void signboard()
 {
     std::string MSIGLOGO;
@@ -25,51 +27,79 @@ void signboard()
     MSIGLOGO += std::string(R"(M  MMM  MMM  M Mb.     .dM M  M MM.     .MM)") + "\n";
     MSIGLOGO += std::string(R"(MMMMMMMMMMMMMM MMMMMMMMMMM MMMM MMMMMMMMMMM)");
     std::cout << MSIGLOGO << std::endl << std::endl;
-    std::cout << "  Musical Symbol Image Generator" << "(ver." << MSIG_VERSION << ")" << std::endl << std::endl;
+    std::cout << "  Musical Symbol Image Generator" << "(ver." << MSIG_VERSION << ")" << std::endl << std::endl << std::endl;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
+    namespace fs = std::filesystem;
+    
     // 플랫폼 설정
 #ifdef _WIN32
     system("chcp 65001");
 #endif
-    
-    std::vector<MSIG::Algorithm::MusicalSymbol> mss;
-    mss.emplace_back("./ms-datasets/test3/staff-a4-0-0.png", true);
-    mss.emplace_back("./ms-datasets/test3/staff-b4-0-0.png", true);
-    mss.emplace_back("./ms-datasets/test3/staff-c5-0-0.png", true);
-    mss.emplace_back("./ms-datasets/test3/note-up-4.png", true);
-    mss.emplace_back("./ms-datasets/test3/note-up-32.png", true);
-    mss.emplace_back("./ms-datasets/test3/accidental-flat.png", true);
-    mss.emplace_back("./ms-datasets/test3/accidental-natural.png", true);
-    mss.emplace_back("./ms-datasets/test3/articulation-staccato.png", true);
-    mss.emplace_back("./ms-datasets/test3/articulation-tenuto.png", true);
-    mss.emplace_back("./ms-datasets/test3/dynamic-crescendo.png", true);
-    mss.emplace_back("./ms-datasets/test3/dynamic-fortepiano.png", true);
-    mss.emplace_back("./ms-datasets/test3/octave-down-end.png", true);
-    
-    MSIG::Algorithm::MusicalSymbol ms = mss[1] + mss[3] + mss[7] + mss[8] + mss[11] + mss[7] + mss[8] + mss[6] + mss[6];
-    ms.show();
-    
-    /*
-    // 1. 프로그램 간판 출력
-    clear();
+    MSIG::Algorithm::DependentSelectionTree("./ms-datasets/symbol-recognition");
+    // 프로그램 이름 출력
     signboard();
     
-    // 2. 작업 선택
-    std::cout << "  작업을 선택하세요." << std::endl;
-    std::cout << "  -----------------" << std::endl;
-    std::cout << "  1. 기본 데이터셋 관리" << std::endl;
-    std::cout << "  2. 데이터셋 생성" << std::endl;
-    std::cout << "  0. 종료" << std::endl;
-    std::cout << "  -----------------" << std::endl;
+    // 현재 디렉토리에 있는 이미지들 조사
+    std::vector<fs::path> vp;
+    for (auto& p : fs::directory_iterator(fs::path(".")))
+    {
+        if (fs::exists(p.path()) &&                 // 존재하는지
+            fs::is_regular_file(p.path()) &&        // 파일인지
+            p.path().extension().string()==".png")  // .png 파일인지
+        {
+            vp.push_back(p.path());
+        }
+    }
+    // 작업 수행
+    if (vp.empty())
+    {
+        std::cout << "  ! 현재 디렉토리에는 \".png\" 이미지가 존재하지 않습니다." << std::endl;
+        std::cout << "  ! 프로그램을 종료합니다." << std::endl << std::endl;
+        return 0;
+    }
+    
+    // 작업 선택
+    std::cout << "  수행할 작업을 선택하세요." << std::endl;
+    std::cout << std::endl;
+    std::cout << "  0 : 추가된 이미지들 config 데이터 생성" << std::endl;
+    std::cout << "  1 : 모든 이미지 config 데이터 수정" << std::endl;
+    std::cout << "  --------------------------------" << std::endl;
     std::cout << "  >> ";
     
-    // 3. 작업 입력 받기
-    int select;
+    // 사용자 선택
+    std::string select;
     std::cin >> select;
-    std::cin.clear();
-    std::cin.ignore(1000, '\n');
-    */
+    std::cout << std::endl;
+    
+    // 작업 수행
+    if (select=="0")
+    {
+        // 1. 새로 추가된 이미지들 __config__ 데이터 생성
+        // 1.1 MusicalSymbol 객체 생성으로 config 생성. (이미 config 데이터가 존재하는 이미지들은 수정 화면이 뜨지 않음.)
+        for (auto& p : vp) {
+            MSIG::Algorithm::MusicalSymbol(p, true);
+        }
+        
+        // 완료
+        std::cout << "  - 작업 완료." << std::endl;
+    }
+    else if (select=="1")
+    {
+        // 2. 모든 이미지들 __config__ 데이터 수정
+        for (auto& p : vp) {
+            MSIG::Algorithm::MusicalSymbol ms(p, false);
+            ms.edit_config();
+        }
+        
+        // 완료
+        std::cout << "  - 작업 완료." << std::endl;
+    }
+    
+    // 프로그램 종료
+    std::cout << std::endl << std::endl << "  - 프로그램을 종료합니다." << std::endl;
+    
     return 0;
 }
