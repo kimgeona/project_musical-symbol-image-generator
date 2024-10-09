936fcb6819895f3d834ea4949f666e0aaeac08d0
Geona Kim (MacOS) <rtguna@icloud.com>
2024-10-09 14:57:15 +0900

    test: 데이터셋 관리 기능 수정 및 추가.

--- src/main.cpp
+++ src/main.cpp
@@ -78,31 +78,16 @@ void todo_0_0() {
     // 데이터셋 경로 생성
     std::filesystem::path datasetPath(datasetName);
     
-    // __rule__.txt, __config__.txt 제거
-    std::cout << "  * 데이터셋의 모든 __rule__.txt와 __config__.txt를 제거합니다." << std::endl;
+    // __config__.txt 제거
+    std::cout << "  * 데이터셋의 모든 __config__.txt를 제거합니다." << std::endl;
     for (auto& p : fs::recursive_directory_iterator(datasetPath)) {
         if (fs::exists(p.path())            &&
             fs::is_regular_file(p.path())   &&
-            (p.path().filename().string()=="__rule__.txt" ||
-             p.path().filename().string()=="__config__.txt"))
+            p.path().filename().string()=="__config__.txt")
         {
             fs::remove(p.path());
         }
     }
-    
-    // __rule__.txt 생성
-    std::cout << "  * 데이터셋의 각각의 폴더마다 __rule__.txt를 생성합니다." << std::endl;
-    std::ofstream((datasetPath/fs::path("__rule__.txt")).string()).close();
-    for (auto& p : fs::recursive_directory_iterator(datasetPath)) {
-        if (fs::exists(p.path())        &&
-            fs::is_directory(p.path()))
-        {
-            std::ofstream((p.path()/fs::path("__rule__.txt")).string()).close();
-        }
-    }
-    
-    // __rule__.txt 기본값 채우기
-    MSIG::Structure::Folder datasetFolder(datasetPath);
     
     // 완료 문구 출력
     std::cout << "  * 완료." << std::endl;
@@ -111,6 +96,29 @@ void todo_0_0() {
     todo_end();
 }
 void todo_0_1() {
+    namespace fs = std::filesystem;
+    
+    // 데이터셋 경로 생성
+    std::filesystem::path datasetPath(datasetName);
+    
+    // __rule__.txt, __config__.txt 제거
+    std::cout << "  * 데이터셋의 모든 __rule__.txt를 제거합니다." << std::endl;
+    for (auto& p : fs::recursive_directory_iterator(datasetPath)) {
+        if (fs::exists(p.path())            &&
+            fs::is_regular_file(p.path())   &&
+            p.path().filename().string()=="__rule__.txt")
+        {
+            fs::remove(p.path());
+        }
+    }
+    
+    // 완료 문구 출력
+    std::cout << "  * 완료." << std::endl;
+    
+    // 프로그램 종료
+    todo_end();
+}
+void todo_0_2() {
     namespace fs = std::filesystem;
     
     // 데이터셋 경로 생성
@@ -132,7 +140,33 @@ void todo_0_1() {
     // 프로그램 종료
     todo_end();
 }
-void todo_0_2() {
+void todo_0_3() {
+    namespace fs = std::filesystem;
+    
+    // 데이터셋 경로 생성
+    std::filesystem::path datasetPath(datasetName);
+    
+    // __rule__.txt 생성
+    std::cout << "  * 데이터셋의 각각의 폴더마다 __rule__.txt를 생성합니다." << std::endl;
+    std::ofstream((datasetPath/fs::path("__rule__.txt")).string()).close();
+    for (auto& p : fs::recursive_directory_iterator(datasetPath)) {
+        if (fs::exists(p.path())        &&
+            fs::is_directory(p.path()))
+        {
+            std::fstream((p.path()/fs::path("__rule__.txt")).string(), std::ios::out|std::ios::app).close();
+        }
+    }
+    
+    // __rule__.txt 기본값 채우기
+    MSIG::Structure::Folder datasetFolder(datasetPath);
+    
+    // 완료 문구 출력
+    std::cout << "  * 완료." << std::endl;
+    
+    // 프로그램 종료
+    todo_end();
+}
+void todo_0_4() {
     namespace fs = std::filesystem;
     
     // 데이터셋 경로 생성
@@ -158,14 +192,18 @@ void todo_0_2() {
 void todo_0()   {
     // 작업 선택
     switch (menu({
-        "데이터셋 초기화",
-        "새로운 이미지들 config 생성",
-        "모든 이미지 config 수정"
+        "이미지 config 전부 제거",
+        "이미지 rule   전부 제거",
+        "이미지 config 생성",
+        "이미지 rule   생성",
+        "이미지 config 수정"
     }, "작업을 선택하세요"))
     {
         case 0: todo_0_0(); break;
         case 1: todo_0_1(); break;
         case 2: todo_0_2(); break;
+        case 3: todo_0_3(); break;
+        case 4: todo_0_4(); break;
     }
 }
 void todo_1()   {
